f871dec53bd9339d12c2fac43a3d8e82eb6e6c53
ayan-afk <79571638+ayan-afk@users.noreply.github.com>
2021-08-12 14:51:42 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,23 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+
+function addUser() {
+
+    user_name = document.getElementById("user_name").value;
+  
+    localStorage.setItem("user_name", user_name);
+    
+      window.location = "kwitter_room.html";
+  }
+  
+  

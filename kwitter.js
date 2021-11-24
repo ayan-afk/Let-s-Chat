f8dfa3fd01cc2fd3697d57814c16a55713b3b3ba
ayan-afk <79571638+ayan-afk@users.noreply.github.com>
2021-11-24 17:35:43 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -10,6 +10,20 @@ signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAj8G8Urq-0ZN85bqcuSXhbQDa5Z9wNdd0",
+  authDomain: "lets-chat-cea7c.firebaseapp.com",
+  projectId: "lets-chat-cea7c",
+  storageBucket: "lets-chat-cea7c.appspot.com",
+  messagingSenderId: "906665755086",
+  appId: "1:906665755086:web:6cae51292005b8a2956b9f",
+  measurementId: "G-5GEHCD22D9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 function addUser() {
 

adc2689485f01ed7e32e1024fe3b6b695a466c80
ayan-afk <79571638+ayan-afk@users.noreply.github.com>
2021-11-24 18:22:15 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,13 +1,17 @@
 const firebaseConfig = {
     apiKey: "AIzaSyAj8G8Urq-0ZN85bqcuSXhbQDa5Z9wNdd0",
     authDomain: "lets-chat-cea7c.firebaseapp.com",
+    databaseURL: "https://lets-chat-cea7c-default-rtdb.firebaseio.com",
     projectId: "lets-chat-cea7c",
     storageBucket: "lets-chat-cea7c.appspot.com",
     messagingSenderId: "906665755086",
     appId: "1:906665755086:web:6cae51292005b8a2956b9f",
     measurementId: "G-5GEHCD22D9"
   };
-firebase.initializeApp(firebaseConfig);
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 document.getElementById("username").innerHTML = "Welcome " + localStorage.getItem("user_name") + "!";
 
